9c71917aa2b6d7eb9b7dd707aa233f8d4ca586f3
krishnedhu <krishnedhu17@gmail.com>
2022-02-12 21:07:37 +0530

    calc app

---
+++ script.js
@@ -0,0 +1,20 @@
+function displayNum(num){
+    result.value+=num
+
+}
+
+function clearBox(){
+    result.value="";
+}
+
+function evaluateExpression(){
+    // let expr=result.value;
+    // let res=eval(expr)
+    // result.value=res
+    result.value=eval(result.value)
+}
+
+function backSpace(){
+    let curValue=result.value;
+    result.value=curValue.slice(0,-1)
+}
